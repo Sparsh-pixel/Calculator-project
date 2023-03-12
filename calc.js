e12b295115ae0a547b209b94bc43ca507d72f50f
Sparsh Prasad <97156719+Sparsh1010@users.noreply.github.com>
2023-03-12 20:26:58 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,24 @@
+let output = document.getElementById('output');
+
+
+function display(num){
+    output.value += num;
+}
+
+function calculate(){
+    try{
+        output.value = eval(output.value);
+    }
+    catch(err)
+    {
+        alert("invalid");
+    }
+}
+
+function clr(){
+    output.value="";
+}
+
+function del(){
+    output.value=output.value.slice(0,-1);
+}
